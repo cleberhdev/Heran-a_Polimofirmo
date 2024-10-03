ff2e753090a6b560b4e521d5f36470f006b5ab7c
devcleberh18 <cleberhenriquelacerdaduarte@gmail.com>
2024-10-03 20:55:55 -0300

    Utilizando Overload no método circulo, e fazendo exemplo no objeto c2 na classe APP

--- herança_Polimorfismo/src/App.java
+++ herança_Polimorfismo/src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Exemplo de como utilizar a sobrecarga:
+        Circulo c = new Circulo();
+        Circulo c2 = new Circulo(2.3f);
+        // OBS -> Sobrecarga é o mesmo atributo com assinaturas diferentes, e com assinaturas diferentes reagem diferente
+        Retangulo r = new Retangulo();
+        Triangulo t = new Triangulo();
+        // Uma classe generica, consigo acessar com as demais classes a partir da herança;
+        c.area = 0.9f;
+        c.perimetro = 0.3f;
+        c.calculaArea();
+        c.calculaPerimetro();
+
+        r.area = 0.0f;
+        r.perimetro = 0.0f;
+        
+        // Exemplo de sobrecarga:
+        c2.calculaArea();
+        c2.calculaArea(2.3f);
     }
 }
--- herança_Polimorfismo/src/Circulo.java
+++ herança_Polimorfismo/src/Circulo.java
@@ -1,8 +1,22 @@
 public class Circulo extends Forma2D{
-    float raio = 0.0f;
+    float raio;
     float PI = 3.1416f;
-    @Override
+    // Polimorfismo por sobregarga -> Não depende da herança, porque de acordo a assinatura ele muda dentro na mesma classe
+    public Circulo(){
+        this.raio = 0;
+    }
+    public Circulo(float raio){
+        this.raio = raio;
+        this.calculaArea();
+    }
+    // Polimofirsmo por Override -> Só funciona se herdar, porque sobrescreve o método na classe filho
+    // Logo depende de herança
+    @Override // Sobreposição
     float calculaArea(){
+        super.area = PI*this.raio*this.raio;
+        return super.area;
+    }
+    float calculaArea(float raio){
         super.area = PI*raio*raio;
         return super.area;
     }
